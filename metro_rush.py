ccc309799be510de15dd9754a89f78cd0b6aa682
HongThuan <guest@localhost.localdomain>
2019-05-12 14:55:10 +0700

    add

--- metro_rush.py
+++ metro_rush.py
@@ -11,10 +11,10 @@ class Station:
 
 class Metro:
     def __init__(self, file_name):
-        self.metro = self.build_metro(file_name)
         self.start = None
         self.end = None
         self.trains = None
+        self.metro = self.build_metro(file_name)
     
     def build_metro(self, file_name):
         output = {}
@@ -25,25 +25,32 @@ class Metro:
                     current_line = line[1:].rstrip()
                     output[current_line] = []
                 elif line.startswith('START'):
-                    self.start = line.split('=')[1]
+                    self.start = line.split('=')[1].split(':')
+                    self.start[1] = int(self.start[1])
                 elif line.startswith('END'):
-                    self.end = line.split('=')[1]
+                    self.end = line.split('=')[1].split(':')
+                    self.end[1] = int(self.end[1])
                 elif line.startswith('TRAINS'):
-                    self.trains = line.split('=')[1]
+                    self.trains = int(line.split('=')[1])
+                    output[self.start[0]][self.start[1]-1].capacity = self.trains
+                    output[self.end[0]][self.end[1]-1].capacity = self.trains
                 else:
                     try:
-                        output[current_line].append(line.split(':')[1].rstrip())
+                        line = line.split(':')
+                        if len(line) > 2:
+                            output[current_line].append(Station(line[1].rstrip(), line=line[3].strip()))
+                        else:
+                            output[current_line].append(Station(line[1].rstrip()))
                     except IndexError:
                         pass
-            print(self.start)
             return output
 
 
 def main():
     graph = Metro('delhi-metro-stations')
-    for x, y in graph.metro.items():
-        print(x)
-        print(y)
+    for x in graph.metro.values():
+        for y in x:
+            print(y.name, 234, y.line, 123)
     print(graph.start)
     print(graph.end)
     print(graph.trains)
