0f2b6e8006938e25b11373b2b4ee4226a4c2dc9e
HongThuan <guest@localhost.localdomain>
2019-05-13 20:05:44 +0700

    add bfs

--- metro_rush.py
+++ metro_rush.py
@@ -26,14 +26,14 @@ class Metro:
                     output[current_line] = []
                 elif line.startswith('START'):
                     self.start = line.split('=')[1].split(':')
-                    self.start[1] = int(self.start[1])
+                    self.start[1] = int(self.start[1]) - 1
                 elif line.startswith('END'):
                     self.end = line.split('=')[1].split(':')
-                    self.end[1] = int(self.end[1])
+                    self.end[1] = int(self.end[1]) - 1
                 elif line.startswith('TRAINS'):
                     self.trains = int(line.split('=')[1])
-                    output[self.start[0]][self.start[1]-1].capacity = self.trains
-                    output[self.end[0]][self.end[1]-1].capacity = self.trains
+                    output[self.start[0]][self.start[1]].capacity = self.trains
+                    output[self.end[0]][self.end[1]].capacity = self.trains
                 else:
                     try:
                         line = line.split(':')
@@ -46,15 +46,77 @@ class Metro:
             return output
 
 
+class node:
+    def __init__(self, position, run, parent):
+        self.position = position
+        self.run = run
+        self.parent = parent
+    
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def find_index(metro, line, position):
+    for x, y in enumerate(metro[line]):
+        if y.name == metro[position[0]][position[1]].name:
+            return x
+
+
+def check_node_anoline(metro, cur_node, open_list):
+    try:
+        print(cur_node.position)
+        if metro[cur_node.position[0]][cur_node.position[1]].line:
+            ano_line = metro[cur_node.position[0]][cur_node.position[1]].line
+            # print(ano_line)
+            index = find_index(metro, ano_line, cur_node.position)
+            # print(index)
+            # print(cur_node.position)
+            # quit()
+            open_list.append(node([ano_line, index], 'a', cur_node))
+            return True
+    except Exception:
+        return False
+
+
+def bfs(metro, start, end):
+    open_list = [node(start, 'a', None)]
+    while open_list:
+        current_node = open_list.pop(0)
+        if current_node.position == end:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        check_node_anoline(metro, current_node, open_list)
+        try:
+            if current_node.position[1] > 0 and current_node.run in ['a', 'l']:
+                new_position = current_node.position.copy()
+                new_position[1] -= 1
+                open_list.append(node(new_position, 'l', current_node))
+        except TypeError:
+            pass
+        
+        try:
+            if current_node.position[1] < len(metro[current_node.position[0]]) - 1 and current_node.run in ['a', 'r']:
+                new_position = current_node.position.copy()
+                new_position[1] += 1
+                open_list.append(node(new_position, 'r', current_node))
+        except TypeError:
+            pass
+
+
 def main():
     graph = Metro('delhi-metro-stations')
-    for x in graph.metro.values():
-        for y in x:
-            print(y.name, 234, y.line, 123)
-    print(graph.start)
-    print(graph.end)
-    print(graph.trains)
-
+    # for x in graph.metro.values():
+    #     for y in x:
+    #         print(y.name, 234, y.line, 123)
+    # print(graph.start)
+    # print(graph.end)
+    # print(graph.trains)
+    print(bfs(graph.metro, graph.start, graph.end))
 
 if __name__ == '__main__':
     main()
