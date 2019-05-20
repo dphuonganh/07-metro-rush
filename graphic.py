add7c70cb6f6790df3c3927282b87478f30650f6
HongThuan <guest@localhost.localdomain>
2019-05-21 00:22:58 +0700

    update graphic, resize image

--- graphic.py
+++ graphic.py
@@ -8,6 +8,30 @@ def write_content(string):
     f.close()
 
 
+def read_content():
+    with open('data', 'r') as f:
+        return f.readlines()
+
+
+class Train:
+    def __init__(self, posx, posy, img):
+        self.posx = posx
+        self.posy = posy
+        self.sprite = self.Sprite(img)
+        self.sprite.x = self.posx
+        self.sprite.y = self.posy
+    
+    def draw(self):
+        self.sprite.draw()
+    
+    def Sprite(self, img):
+        return pyglet.sprite.Sprite(pyglet.image.load(img))
+    
+    def update(self):
+        self.sprite.x = self.posx
+        self.sprite.y = self.posy
+
+
 class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,17 +39,32 @@ class Window(pyglet.window.Window):
         
         temp_background = pyglet.image.load('metro.jpg')
         self.background = pyglet.sprite.Sprite(temp_background)
+
+        self.list_train = []
+        self.list_move = read_content()
+        start = self.list_move[0].split(',')
+        for _ in range(1):
+            self.list_train.append(Train(int(start[0]), int(start[1]), 'train.png'))
+        self.cur_index = 0
     
     def on_draw(self):
         self.clear()
         self.background.draw()
+        for x in self.list_train:
+            x.draw()
         
     def on_mouse_press(self, x, y, button, modifier):
         if button == mouse.LEFT:
-            write_content('{},{}\n'.format(x, y))
+            # write_content('{},{}\n'.format(x, y))
+            self.cur_index += 1
+            for x in self.list_train:
+                temporary = self.list_move[self.cur_index].split(',')
+                x.posx = int(temporary[0])
+                x.posy = int(temporary[1])
     
     def update(self, dt):
-        pass
+        for x in self.list_train:
+            x.update()
 
 
 def main():
